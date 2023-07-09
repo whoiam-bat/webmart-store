01d820d3332ccb090dfec8c36106aa89f23fcbfc
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-07-09 21:29:06 +0300

    Added backend module   - Created Spring Boot project boilerplate   - Added mvn dependency for database migration   - Added mvn dependency for jasypt lib, to encode all secrets in     configuration file.   - Changed config file extension from properties to yml   - Added two first scripts versions for migration

---
+++ webmart-back/src/main/java/ua/com/webmart/WebmartBackApplication.java
@@ -0,0 +1,13 @@
+package ua.com.webmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmartBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmartBackApplication.class, args);
+	}
+
+}
